3a4faab65881150a2ae6ab3182c5bbe57dac04bb
matti124 <mattiafattoruso@gmail.com>
2024-03-09 17:08:55 +0100

    Modificato il primo main

--- src/main/java/ciccio1/MioMain.java
+++ src/main/java/ciccio1/MioMain.java
@@ -4,7 +4,7 @@ public class MioMain {
 
 	public static void main(String[] args) {
 
-		System.out.println("Ciao prima mofica al file");
+		System.out.println("Ciao prima modifica al file");
 	}
 
 }
